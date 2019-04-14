b0aa2d8589af1ad06ea4a1dd4441db90fa622898
root <root@ip-172-31-88-174.ec2.internal>
2019-04-14 22:17:54 +0000

    initial commit

---
+++ src/App.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+<<<<<<< HEAD
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+=======
+>>>>>>> 797351da89cddec7033475109909530c706f064a
